a84a3d367c54832bce2cd1213202aac4f9b47e2c
Chloe Gell <gellert.chloe@gmail.com>
2020-02-02 22:51:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,9 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     @total += price*quantity 
-    @items << title 
+    quantity.times do 
+      @items << title 
+    end 
   end 
   
   def apply_discount
